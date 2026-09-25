c914d9a49953fd033626af31597537b819a7e23e
c11gunworks-lab <c11gunworks@gmail.com>
2026-09-25 14:33:21 -0600

    Update to 4.1

--- 22lr.cs
+++ 22lr.cs
@@ -1,53 +1,52 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using System.Reflection;
+using JetBrains.Annotations;
+using Microsoft.Extensions.Logging;
 using SPTarkov.DI.Annotations;
 using SPTarkov.Server.Core.DI;
 using SPTarkov.Server.Core.Models.Spt.Mod;
-using System.Reflection;
 using Range = SemanticVersioning.Range;
+using Version = SemanticVersioning.Version;
 
 namespace Spt22lr;
 
-public record ModMetadata : AbstractModMetadata
+[UsedImplicitly]
+public record ModMetadata : IModMetadata
 {
-    public override string ModGuid { get; init; } = "com.c11.spt22lr"; 
-    public override string Name { get; init; } = ".22 Long Rifle";
-    public override string Author { get; init; } = "C11";
-    public override SemanticVersioning.Version Version { get; init; } = new("1.5.0");
-
-    public override Range SptVersion { get; init; } = new("^4.0.10");
-
-    public override string License { get; init; } = "MIT";
-    public override bool? IsBundleMod { get; init; } = true;
-
-    public override Dictionary<string, Range>? ModDependencies { get; init; } = new()
+    public string ModGuid { get; init; } = "com.c11.spt22lr";
+    public string Name { get; init; } = ".22 Long Rifle";
+    public string Author { get; init; } = "C11";
+    public List<string>? Contributors { get; init; } = [];
+    public Version Version { get; init; } = new(typeof(ModMetadata).Assembly.GetName().Version!.ToString(3));
+    public Range SptVersion { get; init; } = new("~4.1.6");
+    public bool HasPrepatcher { get; init; } = false;
+    public List<string>? Incompatibilities { get; init; } = [];
+    public Dictionary<string, Range>? ModDependencies { get; init; } = new()
     {
-        { "com.wtt.commonlib", new Range("~2.0.18") }
+        { "com.wtt.commonlib", new Range("~3.0.6") }
     };
-
-    public override string? Url { get; init; }
-    public override List<string>? Contributors { get; init; }
-    public override List<string>? Incompatibilities { get; init; }
+    public string? Url { get; init; }
+    public string License { get; init; } = "MIT";
 }
 
-[Injectable(TypePriority = OnLoadOrder.PostDBModLoader + 2)]
+[Injectable(TypePriority = OnLoadOrder.Preload + 1), UsedImplicitly]
 public class Plugin(
     WTTServerCommonLib.WTTServerCommonLib wttCommon,
-    ILogger<Plugin> log 
+    ILogger<Plugin> log
 ) : IOnLoad
 {
-    public async Task OnLoad()
+    public async Task OnLoadAsync(CancellationToken cancellationToken)
     {
         var assembly = Assembly.GetExecutingAssembly();
 
         foreach (var name in assembly.GetManifestResourceNames())
+        {
             log.LogDebug("[spt_22lr] Embedded resource: {Res}", name);
+        }
 
         await wttCommon.CustomItemServiceExtended.CreateCustomItems(assembly);
         await wttCommon.CustomLocaleService.CreateCustomLocales(assembly);
-    	await wttCommon.CustomAssortSchemeService.CreateCustomAssortSchemes(assembly);
-
+        await wttCommon.CustomAssortSchemeService.CreateCustomAssortSchemes(assembly);
 
         log.LogInformation("Loaded your Plinking Dreams");
-        await Task.CompletedTask;
     }
 }
